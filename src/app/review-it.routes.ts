65dc5601a13322e046368e2ff7c6f8c68fe56a8e
Mathias Pedersen <mk_titan@hotmail.com>
2016-07-13 15:37:52 +0200

    login&signup bit of styling

--- src/app/review-it.routes.ts
+++ src/app/review-it.routes.ts
@@ -7,13 +7,15 @@ import {PhaseConfigComponent} from "./phase-config.component";
 import {TasklistComponent} from "./task-list.component";
 import {PhaseListComponent} from "./phase-list.component";
 import {LoginComponenet} from "./login.component";
+import {SignupComponent} from "./signup.component";
 
 export const routes: RouterConfig = [
   { path: '', component: TasklistComponent },
   { path: 'list', component: TasklistComponent },
   { path: 'phase/:id', component: PhaseConfigComponent },
   { path: 'phases', component: PhaseListComponent },
-  { path: 'login', component: LoginComponenet }
+  { path: 'login', component: LoginComponenet },
+  { path: 'signup', component: SignupComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
