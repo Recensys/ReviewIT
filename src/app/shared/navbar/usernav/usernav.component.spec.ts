eb6226111f5ad3eca747816cca25f57eb56d7460
Mathias Pedersen <mk_titan@hotmail.com>
2016-08-06 11:42:55 +0200

    removed unit test

--- src/app/shared/navbar/usernav/usernav.component.spec.ts
+++ src/app/shared/navbar/usernav/usernav.component.spec.ts
@@ -5,9 +5,4 @@ import { DebugElement } from '@angular/core';
 import { addProviders, async, inject } from '@angular/core/testing';
 import { UsernavComponent } from './usernav.component';
 
-describe('Component: Usernav', () => {
-  it('should create an instance', () => {
-    let component = new UsernavComponent();
-    expect(component).toBeTruthy();
-  });
-});
+
