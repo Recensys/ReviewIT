2874218c6770b8507d26db7b558665a0cae7c19a
Mathias Pedersen <mk_titan@hotmail.com>
2016-09-19 11:35:09 +0200

    updated routes for components

--- src/app/task/task.routes.ts
+++ src/app/task/task.routes.ts
@@ -4,6 +4,6 @@ import { RouterModule }        from '@angular/router';
 import { TaskDetailsComponent, TasklistComponent }    from './';
 
 export const routing: ModuleWithProviders = RouterModule.forChild([
-  
+  { path: '', component: TaskDetailsComponent},
   { path: ':id', component: TasklistComponent},
 ]);
