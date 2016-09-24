b6c4ad65a0d71c380a0640aa21e90577e26ccf99
Mathias Pedersen <mk_titan@hotmail.com>
2016-09-24 22:09:22 +0200

    corrected model ref

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -12,7 +12,7 @@ import {CookieService} from 'angular2-cookie/core';
 import {StageModel} from '../model/stageModel';
 import {Field, StringField, ResourceField, RadioField, NumberField, CheckboxField, BooleanField} from '../field';
 import {Task} from '../model/task';
-import { Studydetails, Study, Study2 } from '../model';
+import { Study,StudyDetails } from '../model';
 
 
 @Injectable()
@@ -139,13 +139,13 @@ export class APIService {
         .map(this.exstractStatusText)
         .catch(this.handleError);
   }
-  public getStudy(id: number) : Observable<Study2> {
+  public getStudy(id: number) : Observable<Study> {
     let url = `${Globals.api}study/${id}`;
     return this.http.get(url, this.AuthOptions())
         .map(this.extractJson)
         .catch(this.handleError);
   }
-  public getStudies() : Observable<Studydetails[]>{
+  public getStudies() : Observable<StudyDetails[]>{
     let url = `${Globals.api}study/`;
     let args = new RequestOptions();
     args.withCredentials = true;
@@ -155,7 +155,7 @@ export class APIService {
         .map(this.extractJson)
         .catch(this.handleError);
   }
-  public saveStudyDetails(id: number, studydetails: Studydetails) : Observable<string> {
+  public saveStudyDetails(id: number, studydetails: StudyDetails) : Observable<string> {
     let body = JSON.stringify({'Id': id, 'StudyDetails': studydetails});
     console.log(body);
     let url = `${Globals.api}study/${id}`;
