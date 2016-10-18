a02037cc200ced9091fd5cb11c2b7c6c9313cf45
Mathias Pedersen <Mathias Pedersen>
2016-10-18 22:02:58 +0200

    md-card layout

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,5 @@
 import { NgModule }      from '@angular/core';
+import { MaterialModule } from '@angular/material'
 
 import { HomeComponent } from './home.component';
 import { routing } from './home.routes';
@@ -6,7 +7,7 @@ import { SharedModule } from '../shared';
 import { StudylistModule } from '../studylist';
 
 @NgModule({
-  imports:      [ routing, StudylistModule, SharedModule ],
+  imports:      [ routing, StudylistModule, SharedModule, MaterialModule ],
   providers:    [ ],
   declarations: [ HomeComponent ],
 })
