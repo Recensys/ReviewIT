092e776337fa61e5e147f60f475906499c2acb7c
Mathias Pedersen <Mathias Pedersen>
2016-11-07 14:02:30 +0100

    impl task fetch

--- src/app/task/task-dashboard.service.ts
+++ src/app/task/task-dashboard.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core'
-import { Http } from '@angular/http'
+import { Http, URLSearchParams } from '@angular/http'
 import { Observable }     from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
 import { ApiHelper } from '../shared'
 import { environment } from '../../environments/environment'
-import { StageDetailsDTO } from '../model/models'
+import { ReviewTaskListDTO } from '../model/models'
 
 @Injectable()
 export class TaskDashboardService{
@@ -16,9 +16,14 @@ export class TaskDashboardService{
         private apihelper: ApiHelper
     ){}
 
-    public get(studyId: number): Observable<StageDetailsDTO[]> {
-        let url = `${environment.api}study/${studyId}/stages`;
-        return this.http.get(url, this.apihelper.JsonOptions())
+    public getTasks(userId: number, stageId: number): Observable<ReviewTaskListDTO> {
+        let url = `${environment.api}tasks`;
+        let params: URLSearchParams = new URLSearchParams();
+        params.set('uid', userId.toString());
+        params.set('sid', stageId.toString());
+        var options = this.apihelper.JsonOptions()
+        options.search = params;
+        return this.http.get(url, options)
             .map(this.apihelper.extractJson)
             .catch(this.apihelper.handleError);
     }
