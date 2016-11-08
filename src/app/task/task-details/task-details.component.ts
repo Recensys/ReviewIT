b7d9e5073c1d51789e805fe1f404fae642bc4150
Mathias Pedersen <Mathias Pedersen>
2016-11-08 11:24:58 +0100

    impl initial

--- src/app/task/task-details/task-details.component.ts
+++ src/app/task/task-details/task-details.component.ts
@@ -1,13 +1,13 @@
 
 import { ActivatedRoute, Params } from '@angular/router';
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { SafeResourceUrl } from '@angular/platform-browser';
 
 import { ReviewTaskListDTO, ReviewTaskDTO, TaskState, FieldDTO, DataType, DataDTO } from '../../model/models';
-import { TaskDetailsService } from './task-details.service' 
+import { TaskDetailsService } from './task-details.service'
 
 @Component({
-    
+
     selector: 'app-task-details',
     templateUrl: 'task-details.component.html',
     styleUrls: ['task-details.component.css'],
@@ -30,12 +30,12 @@ export class TaskDetailsComponent implements OnInit {
 
     constructor(
         private route: ActivatedRoute,
-        private api: TaskDetailsService
-    ) {       
+        private api: TaskDetailsService,
+        private ref: ChangeDetectorRef,
+    ) {
     }
 
     ngOnInit() {
-
         let stub = new ReviewTaskListDTO();
         stub.Tasks = new Array();
         stub.Fields = new Array();
@@ -48,15 +48,15 @@ export class TaskDetailsComponent implements OnInit {
         d1.Value = 'How to dance the tango and other important life achievements'
         t1.Data = new Array();
         t1.Data.push(d1);
-        stub.Tasks.push(t1)
         let t2 = new ReviewTaskDTO()
         let d2 = new DataDTO()
         d2.Value = `Answering life's questions with the sciences, or how to waste your life`
         t2.Data = new Array();
         t2.Data.push(d2);
         stub.Tasks.push(t2)
+        stub.Tasks.push(t1)
 
-        console.log(stub);
+
 
         this.route.params.forEach((params: Params) => {
             this.stageId = +params['id'];
@@ -70,16 +70,20 @@ export class TaskDetailsComponent implements OnInit {
         });
     }
 
-    previous(){
+    previous() {
         let i = this.model.Tasks.indexOf(this.selected);
-        this.selected = this.model.Tasks[i-1];
+        this.selected = null;
+        this.ref.detectChanges();
+        this.selected = this.model.Tasks[i - 1]
     }
 
-    next(){
+    next() {
         let i = this.model.Tasks.indexOf(this.selected);
-        this.selected = this.model.Tasks[i+1];
+        this.selected = null;
+        this.ref.detectChanges();
+        this.selected = this.model.Tasks[i + 1]
     }
-    
+
 
 }
 
