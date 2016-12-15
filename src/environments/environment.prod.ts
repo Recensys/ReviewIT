ed93864c7c236ee5f3f7017e7dd1ef7344e3bb49
mktitan <mk_titan@hotmail.com>
2016-12-15 11:41:30 +0100

    updated prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  api: 'http://reit-backend.azurewebsites.net/',
+  api: 'http://reit-backend.azurewebsites.net/api/',
   blob: 'https://reitblob.blob.core.windows.net/resources/'
 };
