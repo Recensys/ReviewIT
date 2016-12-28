ab6d292e4857786fdc82d16b02f322a783fc0daf
mktitan <mk_titan@hotmail.com>
2016-12-28 11:13:20 +0100

    prepared repo for adal

---
+++ ReviewIT-WebAPI/RecensysCoreRepository/Migrations/20161228100805_adal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecensysCoreRepository.Migrations
+{
+    public partial class adal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserStudyRelations_StudyId",
+                table: "UserStudyRelations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserStageRelation_StageId",
+                table: "UserStageRelation");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StageFieldRelations_FieldId",
+                table: "StageFieldRelations");
+
+            migrationBuilder.DropColumn(
+                name: "CriteriaResultId2",
+                table: "Articles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameWithIdentifyProvider",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NameWithIdentifyProvider",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CriteriaResultId2",
+                table: "Articles",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserStudyRelations_StudyId",
+                table: "UserStudyRelations",
+                column: "StudyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserStageRelation_StageId",
+                table: "UserStageRelation",
+                column: "StageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StageFieldRelations_FieldId",
+                table: "StageFieldRelations",
+                column: "FieldId");
+        }
+    }
+}
